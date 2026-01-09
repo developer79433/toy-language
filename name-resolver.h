8548b79bd9b3ad6fcffb376eae0f95266adb63f6
hvmxalrmhxvarnivpje <hvmxalrmhxvarnivpje@mailinator.com>
2026-01-09 11:41:38 +1100

    Simplify interfaces more.

--- name-resolver.h
+++ name-resolver.h
@@ -2,12 +2,7 @@
 #define TOY_RESOLVE_REFS_H 1
 
 #include "function-types.h"
-#include "str-types.h"
-#include "val-types.h"
-#include "expr-types.h"
-#include "name-resolver-types.h"
 
-void resolver_init(name_resolver *resolver);
-void resolve_names(name_resolver *resolver, toy_function *func);
+void resolve_names(toy_function *func);
 
 #endif /* TOY_RESOLVE_REFS_H */
